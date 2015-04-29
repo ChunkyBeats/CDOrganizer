89a19f6520d6ecbed2807d83c1b0a9061463f17c
Brent Bailey & Cory Ribson <student@epicodus.com>
2015-04-29 14:14:26 -0700

    pass specs for adding artist

--- lib/cdorganizer.rb
+++ lib/cdorganizer.rb
@@ -1,6 +1,8 @@
 class CD
   @@collection = []
 
+  @@collection_by_artist = {}
+
   define_method(:initialize) do |title|
     @title = title
   end
@@ -16,5 +18,19 @@ class CD
   define_singleton_method(:all) do
     @@collection
   end
+
+  define_singleton_method(:clear) do
+    @@collection=[]
+  end
+
+  define_method(:add_artist) do |artist|
+    @@collection_by_artist.store(artist, self)
+  end
+
+  define_singleton_method(:sort_by_artist) do
+    @@collection_by_artist
+  end
+
+
 
 end
--- spec/cdorganizer_spec.rb
+++ spec/cdorganizer_spec.rb
@@ -2,6 +2,10 @@ require('rspec')
 require('cdorganizer')
 
 describe (CD) do
+  before() do
+    CD.clear()
+  end
+
   describe("#title") do
     it("it returns the title of the cd") do
       test_cd = CD.new("Frozen")
@@ -14,8 +18,22 @@ describe (CD) do
     it("saves a CD to the collection") do
       test_cd = CD.new("Frozen")
       test_cd.save()
-      expect(CD.all()).to(eq(test_cd))
+      expect(CD.all()).to(eq([test_cd]))
     end
   end
 
+  describe(".all") do
+    it("should be empty at the beginning") do
+      expect(CD.all()).to(eq([]))
+    end
+  end
+
+  describe("#add_artist") do
+    it("adds an artist for the CD in an array") do
+    test_cd = CD.new("Doggystyle")
+    test_cd.save()
+    test_cd.add_artist("Snoop Dogg")
+    expect(CD.sort_by_artist()).to(eq({"Snoop Dogg" => test_cd}))
+    end
+  end
 end
